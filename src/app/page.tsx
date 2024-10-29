070d73aec4508d3b4989350cfe8f919ecf41c4c9
Taylor Svec <taylorsvec978@yahoo.com>
2024-10-28 23:20:51 -0400

    Tool abstraction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { ImageOff } from "lucide-react";
 import Link from "next/link";
+import ToolCard from "~/components/ToolCard";
+import { AVAILABLE_TOOLS } from "~/schemas";
 
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
@@ -11,7 +14,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex w-full flex-col items-center px-4 pb-24 pt-24 md:px-0 md:pt-36">
+      <main className="flex w-full flex-col items-center gap-24 px-4 pb-24 pt-24 md:px-0 md:pt-36 lg:px-[10vw]">
         <div className="flex flex-col items-center">
           <p className="text-xl font-semibold lg:text-2xl">Welcome to</p>
           <h1 className="bg-gradient-to-br from-blue-500 from-10% via-cyan-500 to-blue-500 to-90% bg-clip-text text-6xl font-bold text-transparent lg:text-8xl">
@@ -21,6 +24,18 @@ export default async function Home() {
             Just some simple, easy-to-use, and cost efficient software tools for
             daily or one-time use.
           </h2>
+        </div>
+
+        <div className="flex w-full flex-col flex-wrap justify-start gap-6 md:flex-row lg:gap-12">
+          {AVAILABLE_TOOLS.map((tool) => (
+            <ToolCard
+              name={tool.name}
+              url={tool.url}
+              color={tool.color}
+              Logo={tool.Logo}
+              description={tool.description}
+            />
+          ))}
         </div>
       </main>
     </HydrateClient>
