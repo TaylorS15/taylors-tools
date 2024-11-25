8d04539ba2791beee0b2e691a748cc595a59697b
Taylor Svec <taylorsvec978@yahoo.com>
2024-11-24 23:38:38 -0500

    fix terms page

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -6,25 +6,28 @@ export default function TermsAndConditions() {
       </h1>
       <p className="text-xl">TERMS OF SERVICE AGREEMENT</p>
       <p className="max-w-3xl">
-        This Terms of Service Agreement ("Agreement") is entered into between
-        "Taylors Tools", a company incorporated under the laws of the state of
-        Delaware, and the user ("User") who intends to use the services offered
-        by Taylors Tools. By accessing or using Taylors Tools' services, User
-        acknowledges that they have read, understood, and agree to be bound by
-        the terms and conditions of this Agreement. If User does not agree to
-        these terms, User may not use the services offered by Taylors Tools.
+        This Terms of Service Agreement (&quot;Agreement&quot;) is entered into
+        between &quot;Taylors Tools&quot;, a company incorporated under the laws
+        of the state of Delaware, and the user (&quot;User&quot;) who intends to
+        use the services offered by Taylors Tools. By accessing or using Taylors
+        Tools&apos; services, User acknowledges that they have read, understood,
+        and agree to be bound by the terms and conditions of this Agreement. If
+        User does not agree to these terms, User may not use the services
+        offered by Taylors Tools.
       </p>
       <ol className="flex max-w-3xl list-inside list-decimal flex-col gap-8">
         <li>
           Definitions
           <ol className="mt-4 flex list-inside list-decimal flex-col gap-4 pl-6">
             <li>
-              "Services" refers to the file hosting and editing services offered
-              by Taylors Tools through miscellaneous web services.
+              &quot;Services&quot; refers to the file hosting and editing
+              services offered by Taylors Tools through miscellaneous web
+              services.
             </li>
             <li>
-              "User Content" refers to any files, data, information, or other
-              content uploaded or transmitted by User through the Services.
+              &quot;User Content&quot; refers to any files, data, information,
+              or other content uploaded or transmitted by User through the
+              Services.
             </li>
           </ol>
         </li>
@@ -57,7 +60,8 @@ export default function TermsAndConditions() {
           <ol className="mt-4 flex list-inside list-decimal flex-col gap-4 pl-6">
             <li>
               User retains all ownership rights in any data, files, or other
-              content ("User Content") uploaded or stored on the Services.
+              content (&quot;User Content&quot;) uploaded or stored on the
+              Services.
             </li>
             <li>
               Taylors Tools does not own or claim any rights to User Content and
@@ -92,12 +96,12 @@ export default function TermsAndConditions() {
           Disclaimer of Warranties
           <ol className="mt-4 flex list-inside list-decimal flex-col gap-4 pl-6">
             <li>
-              Taylors Tools provides its Services on an "as is" and "as
-              available" basis.
+              Taylors Tools provides its Services on an &quot;as is&quot; and
+              &quot;as available&quot; basis.
             </li>
             <li>
-              Taylors Tools does not warrant that the Services will meet User's
-              requirements, be error-free, uninterrupted, or secure.
+              Taylors Tools does not warrant that the Services will meet
+              User&apos;s requirements, be error-free, uninterrupted, or secure.
             </li>
             <li>
               Taylors Tools disclaims all warranties, whether express or
@@ -117,9 +121,9 @@ export default function TermsAndConditions() {
               consequential damages.
             </li>
             <li>
-              Taylors Tools' liability for any claim arising out of or relating
-              to this Agreement shall not exceed the amount paid by User to
-              Taylors Tools for the Services.
+              Taylors Tools&apos; liability for any claim arising out of or
+              relating to this Agreement shall not exceed the amount paid by
+              User to Taylors Tools for the Services.
             </li>
             <li>
               Taylors Tools shall not be liable for any damages arising from the
@@ -134,9 +138,9 @@ export default function TermsAndConditions() {
               User agrees to indemnify, defend, and hold harmless Taylors Tools
               and its officers, directors, employees, agents, and affiliates
               from and against any and all claims, damages, liabilities, costs,
-              and expenses, including reasonable attorneys' fees, arising from
-              or related to User's use of the Services, User Content, or User's
-              breach of this Agreement.
+              and expenses, including reasonable attorneys&apos; fees, arising
+              from or related to User&apos;s use of the Services, User Content,
+              or User&apos;s breach of this Agreement.
             </li>
           </ol>
         </li>
@@ -144,12 +148,12 @@ export default function TermsAndConditions() {
           Termination
           <ol className="mt-4 flex list-inside list-decimal flex-col gap-4 pl-6">
             <li>
-              Taylors Tools may terminate this Agreement and User's access to
-              the Services at any time, with or without cause, by providing
+              Taylors Tools may terminate this Agreement and User&apos;s access
+              to the Services at any time, with or without cause, by providing
               notice to User.
             </li>
             <li>
-              Upon termination, User's access to the Services will be
+              Upon termination, User&apos;s access to the Services will be
               immediately terminated and all User Content will be deleted.
             </li>
             <li>
@@ -183,7 +187,7 @@ export default function TermsAndConditions() {
             </li>
             <li>
               Taylors Tools may assign this Agreement or any of its rights or
-              obligations hereunder without User's prior consent.
+              obligations hereunder without User&apos;s prior consent.
             </li>
             <li>
               User may not assign this Agreement or any of its rights or
